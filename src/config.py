8c86f4551c6c0462074f4a979ca7cfd5d1598ce8
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2025-04-14 19:21:04 -0500

    fix stupid script + make stats look a little nicer

--- src/config.py
+++ src/config.py
@@ -5,3 +5,4 @@ DATABASE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "users.db")
 USE_SSL = True
 SSL_CERT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "cert.pem")
 SSL_KEY = os.path.join(os.path.dirname(os.path.dirname(__file__)), "key.pem")
+USERS_AMMOUNT_IF_TEST = 1000
